65d6d9ba7efa022b80c7b4bb9e8192912152a25e
Anorme <anormeinkumsah@gmail.com>
2024-12-25 21:27:55 +0000

    set up redux with logger middleware

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,20 @@
-// import ReactDOM from 'react-dom'; The new way to import createRoot:
 import { createRoot } from "react-dom/client";
+import { Provider, connect } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import { createLogger } from "redux-logger";
 import './index.css'; 
 import App from './containers/App.jsx';
 import 'tachyons';
-// import registerServiceWorker from './registerServiceWorker';
+import { searchRobots } from "./reducers.js";
 
-
+const logger = createLogger();
+const store = createStore(searchRobots, applyMiddleware(logger));
 
 const root = createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
 
 
-// ReactDOM.render(<App />, document.getElementById('root'));
-// registerServiceWorker();
